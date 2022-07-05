3ce70ea01dfd84e0d742fa2402e535c381f3dd5b
Gabriela Manoel de Lima <gabrielamanoel@MacBook-Air-de-Gabriela.local>
2022-07-05 19:29:58 -0300

    g

---
+++ 0x00/function/Functions.java
@@ -0,0 +1,40 @@
+public class Functions { 
+    // Faça somente 1 coisa 
+    //A 
+    public void saveCash(Income income, Expense expense){ 
+        inRepo.save(income); 
+        outRepo.save(expense); 
+    } 
+        //Somente 1 nível de abstração 
+    //A 
+    @PostMapping("/user") 
+    public User saveUser(User user){ 
+        return userService.save(user); 
+    }    
+ 
+    // Arguments 
+        //B 
+    public void saveUser(String userName, User user, Date date, boolean isAdmin){};  
+ 
+    //  No side effects 
+    //B 
+    public User saveUser(User user){ 
+        return userRepository.save(user); 
+    } 
+    public void setRole(User user){ 
+        user.setRole(user);      
+    } 
+ 
+    // Evite duplicados, extraindo para novos métodos 
+    //B 
+    public Environment getEnvironment(){ 
+        return environmentService.getEnvironment(getVersion()); 
+    } 
+    public Release getRelease(){ 
+        return releaseService.getRelease(getVersion()); 
+    } 
+    private String getVersion(){  
+        return versionService.getVersion(); 
+    } 
+ 
+}
